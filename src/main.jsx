d1ac55503ed8fc22a1c134b7ce6d6a0bdc1b4d46
pmfoysal <pmfoysal@gmail.com>
2024-01-27 13:31:29 +0600

    App Component Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+import React from 'react';
+import App from './app/app';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('pmfoysal')).render(
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>
+);
