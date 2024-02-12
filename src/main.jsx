aa3e7a866ae11d5522942cdd5daf341cabe82530
pmfoysal <pmfoysal@gmail.com>
2024-02-12 14:37:03 +0600

    Custom CSS File Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import './index.css';
 import React from 'react';
 import App from './app/app';
+import './styles/custom.css';
 import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('pmfoysal')).render(
