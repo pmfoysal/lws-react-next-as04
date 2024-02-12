49fe182295b2a923964b7a3885669b1e0b648d26
pmfoysal <pmfoysal@gmail.com>
2024-02-12 14:55:52 +0600

    Home Contents Component Added

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,3 +1,5 @@
+import Home from '@/pages/home';
+
 export default function App() {
-	return <div>App</div>;
+	return <Home />;
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,13 @@
 import { Fragment } from 'react';
 import Footer from '@/components/footer';
 import Header from '@/components/header';
+import Contents from '@/components/home/contents';
 
 export default function Home() {
 	return (
 		<Fragment>
 			<Header />
-
+			<Contents />
 			<Footer />
 		</Fragment>
 	);
