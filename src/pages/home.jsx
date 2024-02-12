9a0c3f6ecaf6220b2eafd04664b781735484d7f1
pmfoysal <pmfoysal@gmail.com>
2024-02-12 14:49:00 +0600

    Home Page Component Added

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import Footer from '@/components/footer';
+import Header from '@/components/header';
+
+export default function Home() {
+	return (
+		<Fragment>
+			<Header />
+
+			<Footer />
+		</Fragment>
+	);
+}
