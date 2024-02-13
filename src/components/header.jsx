44be232608be562ae9e3ffad1a0488cd8b13873a
pmfoysal <pmfoysal@gmail.com>
2024-02-13 20:21:25 +0600

    Search & Category Functionalities Added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,51 +1,50 @@
+import Icons from './icons';
+import { useRef, useState } from 'react';
+import debounce from '@/utilities/debounce';
 import categories from '@/data/categories.json';
 
-export default function Header({ search, setSearch, category, setCategory }) {
+export default function Header({ setSearch, category, setCategory }) {
+	const inputRef = useRef(null);
+	const [isSearch, setIsSearch] = useState(false);
+
+	function handleSearchOpen() {
+		setSearch('');
+		setIsSearch(prev => !prev);
+		inputRef.current.value = '';
+	}
+
+	const handleSearchInput = debounce(event => {
+		setCategory('');
+		setSearch(event.target.value);
+	});
+
+	function handleCategory(value) {
+		return () => {
+			setSearch('');
+			setCategory(value);
+			inputRef.current.value = '';
+		};
+	}
+
 	return (
 		<nav className='border-b border-black py-6 md:py-8'>
 			<div className='container mx-auto flex flex-wrap items-center justify-between gap-6'>
 				<div className='flex items-center space-x-4'>
-					<svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
-						<path
-							d='M8 0.5C12.1421 0.499999 15.5 3.85786 15.5 8C15.5 12.1421 12.1421 15.5 8 15.5C3.85786 15.5 0.5 12.1421 0.5 8C0.499999 3.85786 3.85786 0.5 8 0.5Z'
-							stroke='#00D991'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-						/>
-						<path d='M8 15.4286L8 0.571507' stroke='#00D991' strokeLinecap='round' strokeLinejoin='round' />
-						<path
-							d='M10.4995 14.9999C10.4995 14.9999 12.5715 12.6429 12.5715 8.00008C12.5715 3.35722 10.4995 0.999939 10.4995 0.999939'
-							stroke='#00D991'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-						/>
-						<path
-							d='M5.50049 1.00006C5.50049 1.00006 3.4285 3.35706 3.4285 7.99992C3.4285 12.6428 5.50049 15.0001 5.50049 15.0001'
-							stroke='#00D991'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-						/>
-						<path d='M15.4282 8L0.499512 8' stroke='#00D991' strokeLinecap='round' strokeLinejoin='round' />
-						<path
-							d='M1.5 5C1.5 5 3.5 5 8 5C12.5 5 14.5 5 14.5 5'
-							stroke='#00D991'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-						/>
-						<path
-							d='M1.5 11C1.5 11 3.5 11 8 11C12.5 11 14.5 11 14.5 11'
-							stroke='#00D991'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-						/>
-					</svg>
+					<Icons name='web' />
 					<span>{new Date().toLocaleDateString('en-US', { dateStyle: 'full' })}</span>
 				</div>
 				<a href='/'>
 					<img className='max-w-[100px] md:max-w-[165px]' src='/assets/logo.png' alt='Lws' />
 				</a>
-				<div className='flex items-center space-x-3 lg:space-x-8'>
-					<img src='/assets/icons/search.svg' />
+				<div className='flex items-center space-x-2 lg:space-x-4'>
+					<input
+						type='search'
+						ref={inputRef}
+						placeholder='Search...'
+						onInput={handleSearchInput}
+						className={`h-10 w-[200px] border border-black  rounded-lg px-4 box-border outline-none shadow-none ring-0 ${isSearch || 'invisible'}`}
+					/>
+					<img src='/assets/icons/search.svg' className='cursor-pointer' onClick={handleSearchOpen} />
 				</div>
 			</div>
 			<div className='container mx-auto mt-6'>
@@ -54,7 +53,7 @@ export default function Header({ search, setSearch, category, setCategory }) {
 						<li key={item.id}>
 							<a
 								className={`cursor-pointer ${category === item.value && 'text-[#00d991]'}`}
-								onClick={() => setCategory(item.value)}>
+								onClick={handleCategory(item.value)}>
 								{item.name}
 							</a>
 						</li>
